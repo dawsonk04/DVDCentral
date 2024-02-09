37b3649b2afd6bee83afb87b5624f48bdbe6a75c
Dawson Knudtson <300078204@fvtc.edu>
2024-02-08 20:39:05 -0600

    Added ShipDate to order-Table, along with default data for Order Table

--- DRK.DVDCentral.DB/PostDeploy/DefaultData/Orders.sql
+++ DRK.DVDCentral.DB/PostDeploy/DefaultData/Orders.sql
@@ -1,8 +1,8 @@
 ﻿BEGIN
-	INSERT INTO tblOrder(Id,CustomerId,OrderDate,UserId)
+	INSERT INTO tblOrder(Id,CustomerId,OrderDate,UserId,ShipDate)
 	VALUES
-	(1,1,'2022-10-15',1),
-	(2,2,'2022-10-15',2),
-	(3,3,'2022-10-15',3)
+	(1,1,'2022-10-15',1,'2022-10-15'),
+	(2,2,'2022-10-15',2,'2022-10-16'),
+	(3,3,'2022-10-15',3,'2022-10-20')
 
 END
