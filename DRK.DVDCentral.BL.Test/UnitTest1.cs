4b04f36f1e996d80e9d5c4f82f3d7e8f38a29102
Dawson Knudtson <300078204@fvtc.edu>
2024-02-22 15:00:40 -0600

    added utTableStructure

---
+++ DRK.DVDCentral.BL.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DRK.DVDCentral.BL.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ DRK.DVDCentral.PL/DVDCentralEntities.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DRK.DVDCentral.PL;
+
+public partial class DVDCentralEntities : DbContext
+{
+    public DVDCentralEntities()
+    {
+    }
+
+    public DVDCentralEntities(DbContextOptions<DVDCentralEntities> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<tblCustomer> tblCustomers { get; set; }
+
+    public virtual DbSet<tblDirector> tblDirectors { get; set; }
+
+    public virtual DbSet<tblFormat> tblFormats { get; set; }
+
+    public virtual DbSet<tblGenre> tblGenres { get; set; }
+
+    public virtual DbSet<tblMovie> tblMovies { get; set; }
+
+    public virtual DbSet<tblMovieGenre> tblMovieGenres { get; set; }
+
+    public virtual DbSet<tblOrder> tblOrders { get; set; }
+
+    public virtual DbSet<tblOrderItem> tblOrderItems { get; set; }
+
+    public virtual DbSet<tblRating> tblRatings { get; set; }
+
+    public virtual DbSet<tblUser> tblUsers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=DRK.DVDCentral.DB;Integrated Security=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<tblCustomer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblCusto__3214EC0704619C93");
+
+            entity.ToTable("tblCustomer");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Address)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.City)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.State)
+                .HasMaxLength(2)
+                .IsUnicode(false);
+            entity.Property(e => e.Zip)
+                .HasMaxLength(12)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<tblDirector>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblDirec__3214EC07D00DFC8E");
+
+            entity.ToTable("tblDirector");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<tblFormat>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblForma__3214EC0722A6EE87");
+
+            entity.ToTable("tblFormat");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Description)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<tblGenre>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblGenre__3214EC070B0ABB76");
+
+            entity.ToTable("tblGenre");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Description)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<tblMovie>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblMovie__3214EC078297B9BE");
+
+            entity.ToTable("tblMovie");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Description).IsUnicode(false);
+            entity.Property(e => e.ImagePath).IsUnicode(false);
+            entity.Property(e => e.Title)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<tblMovieGenre>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblMovie__3214EC07B154CD04");
+
+            entity.ToTable("tblMovieGenre");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<tblOrder>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblOrder__3214EC076DC7E759");
+
+            entity.ToTable("tblOrder");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.OrderDate).HasColumnType("datetime");
+            entity.Property(e => e.ShipDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<tblOrderItem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblOrder__3214EC070FBC357F");
+
+            entity.ToTable("tblOrderItem");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<tblRating>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblRatin__3214EC079AD1652A");
+
+            entity.ToTable("tblRating");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Description)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<tblUser>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tblUser__3214EC07423DCF23");
+
+            entity.ToTable("tblUser");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(28)
+                .IsUnicode(false);
+            entity.Property(e => e.UserName)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
