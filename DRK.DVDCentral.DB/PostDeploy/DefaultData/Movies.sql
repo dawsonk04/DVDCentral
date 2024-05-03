3c3e30295ff5ff8df8ec2ba5d29472cf45bd9d53
Dawson Knudtson <300078204@fvtc.edu>
2024-05-03 09:51:54 -0500

    got the card and images to work / display just need to the fix the create --> so it shows up and the edit -- add delete and fix details and then resubmit

--- DRK.DVDCentral.DB/PostDeploy/DefaultData/Movies.sql
+++ DRK.DVDCentral.DB/PostDeploy/DefaultData/Movies.sql
@@ -1,7 +1,7 @@
 ﻿BEGIN
 	INSERT INTO tblMovie(Id,Title,Description,FormatId,DirectorId,RatingId,Cost,InStkQty,ImagePath)
 	VALUES
-	(1,'Sandlot','baseball movie ',1,1,1,57.1,22,'sandlot.png'),
-	(2,'Starwars','Space Movie',2,2,2,52.1,72,'starwars.png'),
-	(3,'Pokemon','Animated Movie',3,3,3,17.1,12,'pokemon.png')
+	(1,'Sandlot','baseball movie ',1,1,1,57.1,22,'BASuggestion.jpg'),
+	(2,'Starwars','Space Movie',2,2,2,52.1,72,'starwars.jpeg'),
+	(3,'Pokemon','Animated Movie',3,3,3,17.1,12,'pokemon.jpg')
 END
