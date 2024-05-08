77b0dc115043af82133e97d72a0cefb556c3666f
Dawson Knudtson <300078204@fvtc.edu>
2024-05-08 14:12:17 -0500

    added shopping cart component, just need to cleanup on cost and things like that, prolly get rid of image path as well, not needed

---
+++ DRK.DVDCentral.UI/ViewComponents/ShoppingCartComponent.cs
@@ -0,0 +1,23 @@
+﻿using DRK.DVDCentral.BL.Models;
+using DRK.DVDCentral.UI.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DRK.ProgDec.UI.ViewComponents
+{
+    public class ShoppingCartComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            if (HttpContext.Session.GetObject<ShoppingCart>("cart") != null)
+            {
+                return View(HttpContext.Session.GetObject<ShoppingCart>("cart"));
+            }
+            else
+            {
+                return View(new ShoppingCart());
+            }
+        }
+
+
+    }
+}
