5e175ed342660d6092d97c410d98ff6ba0e03acf
Dawson Knudtson <300078204@fvtc.edu>
2024-05-09 12:57:20 -0500

    Finished Checkpoint #7

---
+++ DRK.DVDCentral.UI/ViewModels/CustomerVM.cs
@@ -0,0 +1,11 @@
+﻿using DRK.DVDCentral.BL.Models;
+
+namespace DRK.DVDCentral.UI.ViewModels
+{
+    public class CustomerVM
+    {
+        // Checkpoint #7
+        public Customer customer { get; set; }
+        public List<User> users { get; set; } = new List<User>();
+    }
+}
